3984e43cf1fb642d24e42d6ed06170bf2d1b92ef
rofinafiin <rofinafiisr@gmail.com>
2023-11-10 16:33:44 +0700

    getcookie

--- js/template/template.js
+++ js/template/template.js
@@ -1,3 +1,5 @@
+import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
+
 export let URLGeoJson = "https://us-central1-gis-project-401902.cloudfunctions.net/GIS-Project";
 export let urlPostGCF = "https://asia-southeast2-gis-project-401902.cloudfunctions.net/PostCoordinate-GIS";
 export let UrlLogin = "https://asia-southeast2-gis-project-401902.cloudfunctions.net/LoginGeo"
@@ -5,6 +7,7 @@ export let UrlRegister = "https://asia-southeast2-gis-project-401902.cloudfuncti
 export let tableTag="tr";
 export let tableRowClass="content is-small";
 export let token = "cihuy"
+export let cookie = getCookie("Login")
 export let tableTemplate=`
 <td>#NAME#</td>
 <td >#KORDINAT#</td>
