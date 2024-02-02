6cc4c19f9df864f85b601071bb02d1e3b073d729
Rofi Nafiis Zain <rofinafiisr@gmail.com>
2024-02-02 08:55:36 +0700

    change url

--- js/template/template.js
+++ js/template/template.js
@@ -1,6 +1,6 @@
 import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
 
-export let URLGeoJson = "https://us-central1-gis-project-401902.cloudfunctions.net/GIS-Project";
+export let URLGeoJson = "https://asia-southeast2-gis-project-401902.cloudfunctions.net/postgeo";
 export let urlPostGCF = "https://asia-southeast2-gis-project-401902.cloudfunctions.net/PostCoordinate-GIS";
 export let UrlLogin = "https://asia-southeast2-gis-project-401902.cloudfunctions.net/LoginGeo"
 export let UrlRegister = "https://asia-southeast2-gis-project-401902.cloudfunctions.net/RegisterGeo"
